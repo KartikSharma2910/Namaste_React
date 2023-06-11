46f08252e8fe2205a50f8eef9314ca95296cfe09
Kartik Sharma <kartiksharma@Kartiks-MacBook-Air.local>
2023-06-11 17:35:17 +0530

    Episode_Two Igniting our App

---
+++ Episode_Two/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+
+const parent = React.createElement(
+  "div",
+  { id: "parent" },
+  React.createElement("div", { id: "child" }, [
+    React.createElement("h1", {}, "I am a h1 tag!"),
+    React.createElement("h2", {}, "I am a h2 tag!"),
+  ])
+);
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(parent);
